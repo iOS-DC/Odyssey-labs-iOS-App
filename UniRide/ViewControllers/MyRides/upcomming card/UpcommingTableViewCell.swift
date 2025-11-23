23f0b12fc1d15bdb061c97c3768bcb1d8912ab48
Jagpreet Singh <japreet@192.168.1.3>
2025-11-23 22:06:52 +0530

    My ride ui only

---
+++ UniRide/ViewControllers/MyRides/upcomming card/UpcommingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UpcommingTableViewCell.swift
+//  UniRide
+//
+//  Created by Jagpreet Singh on 23/11/25.
+//
+
+import UIKit
+
+class UpcommingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
