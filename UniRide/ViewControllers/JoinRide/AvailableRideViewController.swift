80b278c5d6545ffebdaf97291f749564f61e2ffc
Jagpreet Singh <japreet@Airs-MacBook-Air.local>
2025-11-26 10:39:04 +0530

    added the join ride vc and its code

---
+++ UniRide/ViewControllers/JoinRide/AvailableRideViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AvailableRideViewController.swift
+//  UniRide
+//
+//  Created by Jagpreet Singh on 26/11/25.
+//
+
+import UIKit
+
+class AvailableRideViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
