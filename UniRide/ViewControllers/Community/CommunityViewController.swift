985c3f4a9211fa1d464a6ded1604b1fe4c9e35b9
Rehan-khan0016 <rehan2191.be23@chitkara.edu.in>
2025-11-26 11:35:59 +0530

    community

--- UniRide/ViewControllers/Community/CommunityViewController.swift
+++ UniRide/ViewControllers/Community/CommunityViewController.swift
@@ -1,94 +1,116 @@
-//
-//  CommunityViewController.swift
-//  UniRide
-//
-//  Created by Krish Bahukhandi on 17/11/25.
-//
-
 import UIKit
 
 class CommunityViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet weak var sementedControl: UISegmentedControl!
-    @IBOutlet weak var tableView: UITableView!
-    var showNewPostCell = false
-
-      override func viewDidLoad() {
-          super.viewDidLoad()
-          
-          title = "Community"
-          
-          navigationItem.rightBarButtonItem =
-              UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAddPost))
-          
-          tableView.delegate = self
-          tableView.dataSource = self
-      }
-
-    @objc func didTapAddPost() {
-        showNewPostCell = true
-               tableView.reloadData()
-    }
-
-
-    
+    // MARK: - IBOutlets
  
+    @IBOutlet weak var sementedControl: UISegmentedControl!
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if sementedControl.selectedSegmentIndex == 0 { // Feed
-            return showNewPostCell ? 3 : 2   // 1 new post + 2 feed items
-        } else { // Events
-            return 2 // Or event count
-        }
+    @IBOutlet weak var NewPostContainerView: UIView!
+    @IBOutlet weak var NewPostTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Variables
+    var newPostBottomConstraint: NSLayoutConstraint!
+    
+    var eventPosts: [Post] = [
+        Post(message: "Rangrez 2025", timestamp: "Nov 6, 2025 at 14:00")
+    ]
+    var feedPosts: [Post] = [
+        Post(message: "Planning a weekend trip to Kasauli!", timestamp: "2 hours ago")
+    ]
+    
+    struct Post {
+        let message: String
+        let timestamp: String
     }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        if sementedControl.selectedSegmentIndex == 0 { // Feed section
-            if showNewPostCell && indexPath.row == 0 {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "NewPostCell", for: indexPath)
-                if let textView = cell.viewWithTag(1) as? UITextView {
-                    textView.text = "What's on your mind? Need a ride or offering one?"
-                }
-                return cell
-            } else {
-                let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath)
-                
-                if let username = cell.viewWithTag(2) as? UILabel {
-                    username.text = "Rehan Khan"
-                }
-                if let time = cell.viewWithTag(4) as? UILabel {
-                    time.text = "2 hours ago"
-                }
-                if let message = cell.viewWithTag(5) as? UILabel {
-                    message.text = "Planning a weekend trip to Kasauli!"
-                }
-                return cell
-            }
-            
-        } else { // Event cell
+        title = "Community"
+        NewPostContainerView.isHidden = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAddPost))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Set bottom sheet constraint programmatically
+        NewPostContainerView.translatesAutoresizingMaskIntoConstraints = false
+        newPostBottomConstraint = NewPostContainerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 300)
+        newPostBottomConstraint.isActive = true
+    }
+    
+    // MARK: - Show Bottom Sheet
+    @objc func didTapAddPost() {
+        NewPostContainerView.isHidden = false
+        print("🟢 didTapAddPost triggered successfully")
+
+        let tabBarHeight = tabBarController?.tabBar.frame.height ?? 90
+        UIView.animate(withDuration: 0.3) {
+            self.newPostBottomConstraint.constant = -tabBarHeight
+            self.view.layoutIfNeeded()
+        }
+        
+        NewPostTextField.becomeFirstResponder() // Optional: Auto show keyboard
+    }
+    
+    // MARK: - Close Bottom Sheet
+    @objc func closeNewPostView() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.newPostBottomConstraint.constant = 300
+            self.view.layoutIfNeeded()
+        }) { _ in
+            self.NewPostContainerView.isHidden = true
+        }
+        
+        NewPostTextField.resignFirstResponder()
+    }
+    
+    // MARK: - Post Button Action
+    @IBAction func postButtonTapped(_ sender: UIButton) {
+        guard let text = NewPostTextField.text, !text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty else { return }
+        
+        let newPost = Post(message: text, timestamp: "Just now")
+        eventPosts.insert(newPost, at: 0)
+        tableView.reloadData()
+        
+        NewPostTextField.text = ""
+        sementedControl.selectedSegmentIndex = 1
+        closeNewPostView()
+    }
+    
+    // MARK: - Segment Action
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
+        tableView.reloadData()
+    }
+    
+    // MARK: - TableView Logic
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sementedControl.selectedSegmentIndex == 0 ? feedPosts.count : eventPosts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if sementedControl.selectedSegmentIndex == 0 {  // Feed tab
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath)
+            let post = feedPosts[indexPath.row]
+            (cell.viewWithTag(2) as? UILabel)?.text = "Rehan Khan"
+            (cell.viewWithTag(4) as? UILabel)?.text = post.timestamp
+            (cell.viewWithTag(5) as? UILabel)?.text = post.message
+            return cell
+        } else {  // Events tab
             let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
-            
-            if let title = cell.viewWithTag(2) as? UILabel {
-                title.text = "Rangrez 2025"
-            }
-            if let date = cell.viewWithTag(3) as? UILabel {
-                date.text = "Nov 6, 2025 at 14:00"
-            }
+            let post = eventPosts[indexPath.row]
+            (cell.viewWithTag(2) as? UILabel)?.text = post.message
+            (cell.viewWithTag(3) as? UILabel)?.text = post.timestamp
             return cell
         }
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if sementedControl.selectedSegmentIndex == 0 {
-            if showNewPostCell && indexPath.row == 0 { return 200 }
-            return 160
-        }
-        return 150
+        return sementedControl.selectedSegmentIndex == 0 ? 160 : 150
     }
-    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
-        showNewPostCell = false
-        tableView.reloadData()
-    }
-
-
 }
+
