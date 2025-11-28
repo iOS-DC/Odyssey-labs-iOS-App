886d8349081bdda0589858f81489dbe6a3703375
MuskaanAgg <muskaan0661.be23@chitkara.edu.in>
2025-11-28 10:32:50 +0530

    Update EditProfileViewController.swift

--- UniRide/ViewControllers/Profile/EditProfileViewController.swift
+++ UniRide/ViewControllers/Profile/EditProfileViewController.swift
@@ -1,4 +1,4 @@
-<<<<<<< Updated upstream
+
 //
 //  EditProfileViewController.swift
 //  UniRide
@@ -6,13 +6,11 @@
 //  Created by Student on 25/11/25.
 //
 
-=======
->>>>>>> Stashed changes
+
 import UIKit
 
 class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-<<<<<<< Updated upstream
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var yearTextField: UITextField!
@@ -23,7 +21,7 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
-=======
+
     // MARK: - IBOutlets
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
@@ -37,7 +35,7 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Edit Profile"
->>>>>>> Stashed changes
+
         setupUI()
         loadExistingProfile()
     }
@@ -48,10 +46,9 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         profileImageView.contentMode = .scaleAspectFill
     }
 
-<<<<<<< Updated upstream
-=======
+
     // MARK: - Load Existing Data
->>>>>>> Stashed changes
+
     private func loadExistingProfile() {
         guard let user = UserDataModel.shared.getCurrentUser() else { return }
 
@@ -81,16 +78,15 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         present(picker, animated: true)
     }
 
-<<<<<<< Updated upstream
     // MARK: - Picker Result
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-=======
+
     func imagePickerController(
         _ picker: UIImagePickerController,
         didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]
     ) {
->>>>>>> Stashed changes
+
 
         if let image = info[.editedImage] as? UIImage ??
                        info[.originalImage] as? UIImage {
@@ -99,16 +95,16 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
 
             // Save image temporarily
             if let data = image.jpegData(compressionQuality: 0.85) {
-<<<<<<< Updated upstream
+
                 let url = FileManager.default.temporaryDirectory.appendingPathComponent("profile_temp.jpg")
                 try? data.write(to: url)
                 newPhotoURL = url   // store until Save button tapped
-=======
+
                 let url = FileManager.default.temporaryDirectory
                     .appendingPathComponent("profile_temp.jpg")
                 try? data.write(to: url)
                 newPhotoURL = url
->>>>>>> Stashed changes
+
             }
         }
         dismiss(animated: true)
@@ -119,32 +115,31 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
 
         guard let user = UserDataModel.shared.getCurrentUser() else { return }
 
-<<<<<<< Updated upstream
+
         let updatedFullName = nameTextField.text ?? user.fullName
-=======
+
         let updatedName = nameTextField.text ?? user.fullName
->>>>>>> Stashed changes
+
         let updatedEmail = mailTextField.text ?? user.email
         let updatedPhone = phoneTextField.text ?? user.phone
         let updatedYear = Int(yearTextField.text ?? "") ?? user.year
 
-<<<<<<< Updated upstream
         // Update main fields
         UserDataModel.shared.editCurrentUser(
             fullName: updatedFullName,
             courseName: nil,                 // removed course
-=======
+
         // Update main fields (except phone & email)
         UserDataModel.shared.editCurrentUser(
             fullName: updatedName,
             courseName: nil,
->>>>>>> Stashed changes
+
             year: updatedYear,
             photoURL: newPhotoURL ?? user.photoURL,
             vehicle: user.vehicle
         )
 
-<<<<<<< Updated upstream
+
         // Update phone manually
         if let phone = updatedPhone, !phone.isEmpty {
             if var updatedUser = UserDataModel.shared.getCurrentUser() {
@@ -162,7 +157,7 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         // Email is part of UserProfile struct, but editCurrentUser doesn't update it.
         // If you want to update email too inside the model, tell me and I’ll add it.
 
-=======
+
         // Now update phone + email inside the struct
         if var updatedUser = UserDataModel.shared.getCurrentUser() {
             updatedUser.phone = updatedPhone
@@ -178,7 +173,7 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
         }
 
         // Return to Profile screen
->>>>>>> Stashed changes
+
         navigationController?.popViewController(animated: true)
     }
 }
