26ac5476d122d7d3ad22b4378defec5367148cf6
Jagpreet Singh <japreet@192.168.1.4>
2025-12-22 23:08:09 +0530

    some bugs removed

--- UniRide/Models/MyRideDataModel.swift
+++ UniRide/Models/MyRideDataModel.swift
@@ -1,4 +1,3 @@
-//
 //  ride.swift
 //  UniRide
 //
@@ -376,7 +375,7 @@ final class RideDataModel {
         let myReqs = requests.filter { $0.passengerUserID == userID }
         let passengerFromRequests: [MyTrip] = myReqs.compactMap { req in
             guard let ride = rides.first(where: { $0.id == req.rideID }) else { return nil }
-//            if bookingRideIDs.contains(ride.id) { return nil } // already added via booking
+            if bookingRideIDs.contains(ride.id) { return nil } // already added via booking
             // Exclude cancelled requests from Upcoming
             guard req.status != .cancelled else { return nil }
             guard ride.status == .published || ride.status == .ongoing else { return nil }
@@ -542,5 +541,4 @@ final class RideDataModel {
 
 
 }
-
 
